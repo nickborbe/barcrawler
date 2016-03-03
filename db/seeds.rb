96d55bf0b5c7ada3c311e089728c036f8f7675de
Nick Borbe <nickborbe@gmail.com>
2016-03-03 14:59:11 -0500

    remove video controls, change map height, re seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
  
 
 
-Venue.create name: "Club Face", address: "120 SW 8th street Miami, FL", url: "/assets/Alto promo.mp4"
+Venue.create name: "Club Face", address: "120 SW 16th street Miami, FL", url: "/assets/Alto promo.mp4"
 Venue.create name: "Geo Kayes", address: "135 SW 8th street Miami, FL", url: "/assets/Amnesia nightclub.mp4"
 Venue.create name: "Monte's", address: "11640 SW 128th Court Miami, FL", url: "/assets/Aura Nightclub Kansas City.mp4"
 Venue.create name: "Brews Room", address: "12055 SW 117th Ave Miami, FL", url: "/assets/beach bar 2.mp4"
 Venue.create name: "Chachkies", address: "140 SW 8th street Miami, FL", url: "/assets/Beach Bar.mp4"
 Venue.create name: "John Martins", address: "253 Miracle Mile Miami, FL", url: "/assets/Blurred Wild Dancefloor.mp4"
 Venue.create name: "Chillers", address: "225 SW 8th street Miami, FL", url: "/assets/Dark Club Scene.mp4"
-Venue.create name: "Nutters", address: "330 SW 8th street Miami, FL", url: "/assets/Dj PoP Dj Man.mp4"
+Venue.create name: "Nutt Haus", address: "330 SW 8th street Miami, FL", url: "/assets/Dj PoP Dj Man.mp4"
 Venue.create name: "La Nena", address: "330 SW 10th street Miami, FL", url: "/assets/ibiza nightclub.mp4"
 Venue.create name: "Beachko's", address: "729 SW 1st Ave Miami, FL", url: "/assets/Nightclub Smoke Machine.mp4"
 Venue.create name: "Flanitans", address: "180 SW 16th street Miami, FL", url: "/assets/People's beach bar.mp4"
